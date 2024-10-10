30fcbe4e3e2bbadf4986d35080e8b798e6a17996
Yahor Kazlou <yahor.kazlou.94@gmail.com>
2024-10-10 22:36:46 +0300

    Added navArray in index.js

---
+++ index.js
@@ -0,0 +1,72 @@
+const navArray = [
+  {
+    title: "Wht Spring",
+    items: [
+      { name: "Overview" },
+      { name: "Microservices" },
+      { name: "Reactive" },
+      { name: "Event Driven" },
+      { name: "Cloud" },
+      { name: "Web Applications" },
+      { name: "Serverless" },
+      { name: "Batch" },
+    ],
+  },
+  {
+    title: "Learn",
+    items: [
+      { name: "Overview" },
+      { name: "Quickstart" },
+      { name: "Guides" },
+      { name: "Blog" },
+    ],
+  },
+  {
+    title: "Projects",
+    items: [
+      { name: "Overview" },
+      { name: "Spring Boot" },
+      { name: "Spring Framowork" },
+      { name: "Spring Cloud" },
+      { name: "Spring Cloud Data Flow" },
+      { name: "Spring Data" },
+      { name: "Spring Integration" },
+      { name: "Spring Batch" },
+      { name: "Spring Security" },
+      { name: "Spring AI" },
+      { name: "View all projects" },
+      { name: "DEVELOPMENT TOOLS", type: "subheader" },
+      { name: "Spring Tools 4" },
+      { name: "Spring Initializr", icon: null },
+    ],
+  },
+  {
+    title: "Academy",
+    items: [{ name: "Courses" }, { name: "Get Certified" }],
+  },
+  {
+    title: "Solutions",
+    items: [
+      { name: "Overview" },
+      { name: "Tanzu Spring" },
+      { name: "Spring Consulting" },
+      { name: "Spring Academy For Teams" },
+      { name: "Security Advisories" },
+    ],
+  },
+  {
+    title: "Community",
+    items: [{ name: "Overview" }, { name: "Events" }, { name: "Autors" }],
+  },
+];
+
+const collection = document.getElementsByClassName("example");
+
+window.onload = function () {
+  yourFunction(param1, param2);
+};
+
+document.forms[0].appendChild(document.createElement("input")).className =
+  "foo";
+
+classList;
