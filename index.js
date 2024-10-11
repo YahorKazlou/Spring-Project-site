d70fbf570e31ce53dc453274c503796029b3d18a
Yahor Kazlou <yahor.kazlou.94@gmail.com>
2024-10-11 11:57:11 +0300

    Added navbar-menu-mobile | Fixed navbar-dropdown

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const navArray = [
   {
-    title: "Wht Spring",
+    title: "Why Spring",
     items: [
       { name: "Overview" },
       { name: "Microservices" },
@@ -34,7 +34,7 @@ const navArray = [
       { name: "Spring Batch" },
       { name: "Spring Security" },
       { name: "Spring AI" },
-      { name: "View all projects" },
+      { name: "View all projects", type: "viewall" },
       { name: "DEVELOPMENT TOOLS", type: "subheader" },
       { name: "Spring Tools 4" },
       { name: "Spring Initializr", icon: null },
@@ -60,6 +60,20 @@ const navArray = [
   },
 ];
 
+const classNamesDesktop = {
+  navbarItem: "navbar-item has-dropdown is-hoverable navbar-group-item",
+  navbarItemText: "navbar-link",
+  navbarItemChildren: "navbar-dropdown is-boxed",
+  navbarChildContent: "navbar-item",
+};
+
+const classNamesMobile = {
+  navbarItem: "navbar-item has-dropdown is-hoverable navbar-group-item",
+  navbarItemText: "navbar-link",
+  navbarItemChildren: "navbar-dropdown is-boxed",
+  navbarChildContent: "navbar-item",
+};
+
 const createDOMElement = ({ tag, className, textContent }) => {
   const element = document.createElement(tag);
   if (className) {
@@ -72,35 +86,45 @@ const createDOMElement = ({ tag, className, textContent }) => {
   return element;
 };
 
-const populateDesktopNavbar = () => {
-  let navbarMenu = document.getElementsByClassName("navbar-menu")[0];
+const populateNavbar = (menuClass, classNames, isMenuSectionClickable) => {
+  let navbarMenu = document.getElementsByClassName(menuClass)[0];
   navArray.forEach((item) => {
     const navbarItem = createDOMElement({
       tag: "div",
-      className: "navbar-item has-dropdown is-hoverable navbar-group-item",
+      className: classNames.navbarItem,
     });
+    // make this clickable
     const navbarItemText = createDOMElement({
       tag: "span",
-      className: "navbar-link",
+      className: classNames.navbarItemText,
       textContent: item.title,
     });
+    if (isMenuSectionClickable) {
+      navbarItemText.addEventListener(
+        "click",
+        onMenuSectionClick(navbarItemText),
+      ); // ? false
+    }
     navbarItem.appendChild(navbarItemText);
 
     const navbarItemChildren = createDOMElement({
       tag: "ul",
-      className: "navbar-dropdown is-boxed",
+      className: classNames.navbarItemChildren,
     });
 
     item.items.forEach((childItem) => {
       const navbarChild = createDOMElement({
         tag: "li",
       });
-      const navbarChildLink = createDOMElement({
-        tag: "a",
-        className: "navbar-item",
+
+      const navbarChildContent = createDOMElement({
+        tag: childItem.type === "subheader" ? "div" : "a",
+        className:
+          classNames.navbarChildContent +
+          (childItem.type === "viewall" ? " is-link mb-5" : ""),
         textContent: childItem.name,
       });
-      navbarChild.appendChild(navbarChildLink);
+      navbarChild.appendChild(navbarChildContent);
       navbarItemChildren.appendChild(navbarChild);
     });
     navbarItem.appendChild(navbarItemChildren);
@@ -110,5 +134,37 @@ const populateDesktopNavbar = () => {
 };
 
 window.onload = function () {
-  populateDesktopNavbar();
+  const isDesktop = window.innerWidth > 1000;
+  populateNavbar(
+    isDesktop ? "navbar-menu-desktop" : "navbar-menu-mobile",
+    isDesktop ? classNamesDesktop : classNamesMobile,
+    !isDesktop,
+  );
+};
+
+let isMenuOpen = false;
+
+const onBurgerMenuClick = () => {
+  const menu = document.getElementById("navMenu");
+  menu.style.transform = `translateY(${isMenuOpen ? "-100%" : "0"})`;
+  const burgerIcon = document.getElementsByClassName("hamburger-react")[0];
+  burgerIcon.style.color = isMenuOpen ? "#4a4a4a" : "white";
+  isMenuOpen = !isMenuOpen;
+};
+
+const onMenuSectionClick = (node) => {
+  let isCurrentMenuOpened = false;
+
+  return () => {
+    if (isCurrentMenuOpened) {
+      node.parentElement.classList.remove("subitems-show");
+    } else {
+      const allMenus = document.getElementsByClassName("has-dropdown");
+      Array.prototype.forEach.call(allMenus, (menu) => {
+        menu.classList.remove("subitems-show");
+      });
+      node.parentElement.classList.add("subitems-show");
+    }
+    isCurrentMenuOpened = !isCurrentMenuOpened;
+  };
 };
